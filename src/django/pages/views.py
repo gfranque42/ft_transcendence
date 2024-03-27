43a047f3da592ca7d5fb06b62dc321c65d936408
Stanouuu <stanislas.barrage@gmail.com>
2024-03-27 11:24:07 +0100

    ajusted speeds

--- src/django/pages/views.py
+++ src/django/pages/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return render(request, 'pages/index.html')
+
+def login(request):
+    return render(request, 'pages/login.html')
+
+def signup(request):
+    return render(request, 'pages/signup.html')
