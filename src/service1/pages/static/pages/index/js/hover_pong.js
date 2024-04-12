f6ab7ea3b3297912ed36da81e0e3fd5c9a5136b0
gfranque42 <gfranque@student.42.fr>
2024-04-12 16:53:10 +0200

    🐛 fix: new architecture fixed

--- src/service1/pages/static/pages/index/js/hover_pong.js
+++ src/service1/pages/static/pages/index/js/hover_pong.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const indexPong = document.querySelector('.index-pong');
     const indexSudoku = document.querySelector('.index-sudoku');
 
+<<<<<<< HEAD
 
     if (indexSudoku && indexPong)
     {
@@ -15,4 +16,15 @@ document.addEventListener("DOMContentLoaded", function() {
             indexSudoku.classList.remove('shrunken-pong');
         });
     }
+=======
+    indexPong.addEventListener('mouseenter', function() {
+        indexPong.classList.add('scaled-pong');
+        indexSudoku.classList.add('shrunken-pong');
+    });
+
+    indexPong.addEventListener('mouseleave', function() {
+        indexPong.classList.remove('scaled-pong');
+        indexSudoku.classList.remove('shrunken-pong');
+    });
+>>>>>>> 248fe0e (🐛 fix: new architecture fixed)
 });
