61d58f10ff2d88b90601de6a415582cf1831d6d3
Stanouuu <stanislas.barrage@gmail.com>
2024-04-15 23:01:09 +0200

    🚧 feat: made the sign up page not look like shit

--- src/service3/pages/views.py
+++ src/service3/pages/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
+from .forms import CreateUserForm
+
 # Create your views here.
 def register(response):
     if response.method == "POST":
-        form = UserCreationForm(response.POST)
+        form = CreateUserForm(response.POST)
         if form.is_valid():
             form.save()
     else:
-        form = UserCreationForm()
-    form = UserCreationForm()
+        form = CreateUserForm()
+    print(form)
     return render(response, "register/register.html", {"form":form})
