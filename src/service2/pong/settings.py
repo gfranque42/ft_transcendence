253dc66e2b3adf9d1d632a94bfc4291399293634
gfranque42 <gfranque@student.42.fr>
2024-04-16 19:10:09 +0200

    🔨  build: setup for the websocket ok

--- src/service2/pong/settings.py
+++ src/service2/pong/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+	'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -128,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ASGI_APPLICATION = "pong.asgi.application"
