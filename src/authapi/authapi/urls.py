8f9e9a98cdb07a735b65b780752b9f43281169d8
Stanouuu <stanislas.barrage@gmail.com>
2024-06-28 17:10:18 +0200

    🚧 authentication: tested and rebasing with main

--- src/authapi/authapi/urls.py
+++ src/authapi/authapi/urls.py
@@ -2,7 +2,7 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^login$', views.login, name='login'),  # Endpoint for user login
-    re_path(r'^register$', views.RegisterForm.as_view(), name='register'),  # Endpoint for registration
-    re_path(r'^test_token$', views.test_token, name='test_token'),  # Endpoint for testing token
+    re_path(r'^auth/login$', views.login, name='login'),  # Updated endpoint for user login
+    re_path(r'^auth/register$', views.RegisterForm.as_view(), name='register'),  # Endpoint for registration
+    re_path(r'^auth/test_token$', views.test_token, name='test_token'),  # Endpoint for testing token
 ]
