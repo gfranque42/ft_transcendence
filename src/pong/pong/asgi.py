983e22d4f8a095b2c1eac2a7471652f2c7853e2d
gfranque42 <gfranque@student.42.fr>
2024-07-18 16:55:10 +0200

    🚧 add: redis wip

--- src/pong/pong/asgi.py
+++ src/pong/pong/asgi.py
@@ -9,13 +9,20 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+
+from game.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pong.settings")
-
+django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
 	{
 		"http": get_asgi_application(),
+		"websocket": AllowedHostsOriginValidator(
+			AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+		)
 	}
 )
