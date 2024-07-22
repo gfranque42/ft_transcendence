fb3c5ffadaf96dfa936e3bf0c0b0f8e574f7a8c6
gfranque42 <gfranque@student.42.fr>
2024-07-22 15:26:52 +0200

    🚧 add: consumer wip

--- src/pong/game/consumers.py
+++ src/pong/game/consumers.py
@@ -1,18 +1,17 @@
 import json
 
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-class	ChatConsumer(WebsocketConsumer):
-	def connect(self):
+class	ChatConsumer(AsyncWebsocketConsumer):
+	async def connect(self):
 		self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
 		self.room_group_name = f"chat_{self.room_name}"
 
-		async_to_sync(self.channel_layer.group_add)(
+		await self.channel_layer.group_add(
 			self.room_group_name, self.channel_name
 		)
 
-		self.accept()
+		await self.accept()
 
 	def disconnect(self, close_code):
 		async_to_sync(self.channel_layer.group_discard)(
