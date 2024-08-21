1585df2c854007c9211fe749d143fa950f808392
gfranque42 <gfranque@student.42.fr>
2024-08-21 18:18:55 +0200

    🚧 feat: add the pong setup in consumers

--- src/pong/app/consumers.py
+++ src/pong/app/consumers.py
@@ -3,18 +3,32 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync, sync_to_async
 from .models import Room, Player
+from .pong import *
 
 class	PongConsumer(AsyncWebsocketConsumer):
+	def	__init__(self, *args, **kwargs):
+		super().__init(*args, **kwargs)
+		self.room_name = None
+		self.room_group_name = None
+		self.party_state = "waiting"
+		self.game_state = {
+		'ball': Ball(Vec2(45, 45), Vec2(10, 10), Vec2(10, 0), 10),
+		'paddlel': Paddle(Vec2(5, 35),Vec2(10, 30), 0),
+		'paddler': Paddle(Vec2(85, 35),Vec2(10, 30), 0),
+		}
+
 	async def connect(self):
 		self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
 		self.room_group_name = "pong_%s" % self.room_name
 		
 		try:
 			room = await self.get_room(self.room_name)
-			if await room_is_full(room)
+			if await self.room_is_full(room):
 				await self.close()
-			else
+			else:
 				await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+				# Search for the player name from Stan
+				await self.add_player_to_room(self.room)
 				await self.accept()
 		except:
 			await self.close()
