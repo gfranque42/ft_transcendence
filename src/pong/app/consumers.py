3abcc8d4cfe7f0eee29bd286d3acc829842bd6e3
gfranque42 <gfranque@student.42.fr>
2024-08-29 17:14:20 +0200

    🚧 feat: multi

--- src/pong/app/consumers.py
+++ src/pong/app/consumers.py
@@ -6,32 +6,14 @@ from .models import Room, Player
 from .pong import *
 
 class	PongConsumer(AsyncWebsocketConsumer):
-	def	__init__(self, *args, **kwargs):
-		super().__init(*args, **kwargs)
-		self.room_name = None
-		self.room_group_name = None
-		self.party_state = "waiting"
-		self.game_state = {
-		'ball': Ball(Vec2(45, 45), Vec2(10, 10), Vec2(10, 0), 10),
-		'paddlel': Paddle(Vec2(5, 35),Vec2(10, 30), 0),
-		'paddler': Paddle(Vec2(85, 35),Vec2(10, 30), 0),
-		}
-
 	async def connect(self):
 		self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
 		self.room_group_name = "pong_%s" % self.room_name
-		
-		try:
-			room = await self.get_room(self.room_name)
-			if await self.room_is_full(room):
-				await self.close()
-			else:
-				await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-				# Search for the player name from Stan
-				await self.add_player_to_room(self.room)
-				await self.accept()
-		except:
-			await self.close()
+
+		# Join room group
+		await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+
+		await self.accept()
 
 	async def disconnect(self, close_code):
 		# Leave room group
@@ -53,20 +35,3 @@ class	PongConsumer(AsyncWebsocketConsumer):
 
 		# Send message to WebSocket
 		await self.send(text_data=json.dumps({"message": message}))
-
-	@sync_to_async
-	def get_room(self, room_name):
-		return (Room.objects.get(url=room_name))
-
-	@sync_to_async
-	def room_is_full(self, room):
-		return (room.players.count() >= room.maxPlayers)
-
-	@sync_to_async
-	def	add_player_to_room(self, room):
-		player, created = Player.objects.get_or_create(username="current_player")
-		room.players.add(player)
-
-
-
-
