402565f07816ecc5447488878cdee93fa47b6cfb
Stanouuu <stanislas.barrage@gmail.com>
2024-10-10 19:29:38 +0200

    stan is going to lil crazy mode

--- src/home/pages/static/pages/index/views/waiting_sudoku.js
+++ src/home/pages/static/pages/index/views/waiting_sudoku.js
@@ -1,6 +1,8 @@
 import abstractviews from "./abstractviews.js";
 import {navigateToInstead} from "../js/index.js";
 import {DNS} from "../js/dns.js";
+import {loadCSS} from "../js/loadCSS.js";
+
 
 export let csrfToken = null;
 
@@ -19,6 +21,15 @@ export default class extends abstractviews {
             navigateToInstead("/login/");
             return ;
         }
+
+        const cssFiles = [ 
+            'https://'+DNS+':8083/sudokubattle/static/css/lobby.css?request_by=Home',
+            'https://'+DNS+':8083/sudokubattle/static/css/navbar.css?request_by=Home',
+            'https://'+DNS+':8083/sudokubattle/static/css/sudoku.css?request_by=Home',
+            'https://'+DNS+':8083/sudokubattle/static/css/waiting.css?request_by=Home',
+        ];
+
+        cssFiles.forEach(url => loadCSS(url));
 
         const response = await fetch('https://'+DNS+':8083/sudokubattle/api/waiting_room/?request_by=Home');
         const tempContentHtml = await response.text();
